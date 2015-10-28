edbb053e3c84cfa2d61d7ad2faeb88212151d974
Andres Yepes <yepes07@gmail.com>
2015-10-27 23:34:56 -0400

    Scan different regions

--- ChimpNoise/FirstViewController.m
+++ ChimpNoise/FirstViewController.m
@@ -9,7 +9,9 @@
 #import "FirstViewController.h"
 #import <CoreLocation/CoreLocation.h>
 
-#define BEACON_UUID @"0D24BE5C-FE93-707E-041E-CEFBCACA4D2D"
+#define BEACON_UUID_1 @"0D24BE5C-FE93-707E-041E-CEFBCACA4D2D"
+#define BEACON_UUID_2 @"4D3B99C4-3857-D6C3-987A-BA2DA9C4AA19"
+
 
 @interface FirstViewController () <CLLocationManagerDelegate>
 
@@ -32,20 +34,28 @@
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     
-//Show Dialog to approve Beacon Monitoring and Ranging
-    [self.locationManager requestAlwaysAuthorization];
-    
-//Init Region
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:BEACON_UUID];
-    CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
-                                                                identifier:@"com.nsscreencast.beaconfun.region"];
-    region.notifyOnEntry = YES;
-    region.notifyOnExit = YES;
-    region.notifyEntryStateOnDisplay = YES;
+//Init Region 1
+    NSUUID *uuid1 = [[NSUUID alloc] initWithUUIDString:BEACON_UUID_1];
+    CLBeaconRegion *region1 = [[CLBeaconRegion alloc] initWithProximityUUID:uuid1
+                                                                 identifier:@"chimpnoise.one"];
+    region1.notifyOnEntry = YES;
+    region1.notifyOnExit = YES;
+    region1.notifyEntryStateOnDisplay = YES;
+
+//Init Region 2
+    NSUUID *uuid2 = [[NSUUID alloc] initWithUUIDString:BEACON_UUID_2];
+    CLBeaconRegion *region2 = [[CLBeaconRegion alloc] initWithProximityUUID:uuid2
+                                                                 identifier:@"chimpnoise.two"];
+    region2.notifyOnEntry = YES;
+    region2.notifyOnExit = YES;
+    region2.notifyEntryStateOnDisplay = YES;
     
 //start Monitoring
-    [self.locationManager startMonitoringForRegion:region];
-    [self.locationManager requestStateForRegion:region];
+    [self.locationManager startMonitoringForRegion:region1];
+    
+    [self.locationManager performSelector:@selector(requestStateForRegion:) withObject:region1 afterDelay:1];
+    [self.locationManager performSelector:@selector(startMonitoringForRegion:) withObject:region2 afterDelay:2];
+    [self.locationManager performSelector:@selector(requestStateForRegion:) withObject:region2 afterDelay:3];
     
 //Monkey Patch to range Beacon on Background
 //    self.locationManager2 = [[CLLocationManager alloc] init];
@@ -54,7 +64,7 @@
 //    [self.locationManager2 startUpdatingLocation];
 
     
-//------------------------------------------
+//Init Model
     self.placesByBeacons = @{
                              @"1:1":@{
                                      @"title": @"Netshoes",
@@ -67,56 +77,85 @@
                                      @"image": @"http://www.sprint.com/landings/iphone-forever/images/iphone6s_lockup.jpg"
                                      }
                              };
+    NSLog(@"viewDidLoad");
+    
+    //Show Dialog to approve Beacon Monitoring and Ranging
+    [self.locationManager requestAlwaysAuthorization];
 }
 
 #pragma mark - CLLocationManagerDelegate
 
 - (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region {
     if (state == CLRegionStateInside) {
-        
-        CLBeaconRegion *beaconRegion = (CLBeaconRegion *)region;
-        [self.locationManager startRangingBeaconsInRegion:beaconRegion];
-        NSLog(@"didDetermineState");
+
+        [self.locationManager startRangingBeaconsInRegion:(CLBeaconRegion *)region];
+
     }
 }
 
 - (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    
+    NSLog(@"didEnterRegion");
+    
     if ([region isKindOfClass:[CLBeaconRegion class]]) {
         
+        CLBeaconRegion *beaconRegion = (CLBeaconRegion *)region;
         UILocalNotification *notification = [UILocalNotification new];
-        notification.alertAction = @"See";
-        notification.alertBody = @"Noise!";
-        notification.alertTitle = @"Noise Title";
+        
+        if ([beaconRegion.identifier isEqualToString:@"chimpnoise.one"]) {
+            notification.alertAction = @"See";
+            notification.alertBody = @"Shoes Discount!";
+            notification.alertTitle = @"Converse";
+        }
+        else {
+            notification.alertAction = @"See";
+            notification.alertBody = @"Iphone offer!";
+            notification.alertTitle = @"Apple Store";
+        }
+        
+        notification.soundName = UILocalNotificationDefaultSoundName;
         [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
         
-        CLBeaconRegion *beaconRegion = (CLBeaconRegion *)region;
         [self.locationManager startRangingBeaconsInRegion:beaconRegion];
-        NSLog(@"didEnterRegion");
+
     }
 }
 
 - (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
+    
+    NSLog(@"didExitRegion");
+    
     if ([region isKindOfClass:[CLBeaconRegion class]]) {
         
         CLBeaconRegion *beaconRegion = (CLBeaconRegion *)region;
         [self.locationManager stopRangingBeaconsInRegion:beaconRegion];
-        NSLog(@"didExitRegion");
     }
 }
 
 
 - (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
     NSLog(@"didRangeBeacons");
-    CLBeacon *nearestBeacon = (CLBeacon *)[beacons firstObject];
-    NSString *beaconKey = [NSString stringWithFormat:@"%@:%@", nearestBeacon.major, nearestBeacon.minor];
-    NSDictionary *nearestBeaconDetails = [self.placesByBeacons objectForKey:beaconKey];
+    NSLog(@"Region: %@", region.identifier);
+    NSLog(@"# Beacons: %ld", [beacons count]);
     
-    NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:[nearestBeaconDetails objectForKey:@"image"]]];
-    self.imageView.image = [UIImage imageWithData:imageData];
-    
-    self.titleLabel.title = [nearestBeaconDetails objectForKey:@"title"];
-    self.titleLabel.prompt = [nearestBeaconDetails objectForKey:@"prompt"];
-    
+    if (beacons == nil || [beacons count] == 0) {
+    }
+    else {
+        CLBeacon *nearestBeacon = (CLBeacon *)[beacons firstObject];
+        NSString *beaconKey = [NSString stringWithFormat:@"%@:%@", nearestBeacon.major, nearestBeacon.minor];
+        NSDictionary *nearestBeaconDetails = [self.placesByBeacons objectForKey:beaconKey];
+        
+        NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:[nearestBeaconDetails objectForKey:@"image"]]];
+        self.imageView.image = [UIImage imageWithData:imageData];
+        
+        self.titleLabel.title = [nearestBeaconDetails objectForKey:@"title"];
+        self.titleLabel.prompt = [nearestBeaconDetails objectForKey:@"prompt"];
+    }
+}
+
+-(void)locationManager:(CLLocationManager *)manager monitoringDidFailForRegion:(CLRegion *)region withError:(NSError *)error{
+    NSLog(@"monitorDidFailForRegion");
+    NSLog(@"with error: %ld ||| %@ ||| %@", error.code, error.domain, error.localizedDescription);
 }
 
 #pragma mark - Utility Methods
