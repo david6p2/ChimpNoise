2c21bacd18ce7e3fc491182f858da5ea5fe2b85c
Andres Yepes <yepes07@gmail.com>
2016-01-03 17:55:30 -0500

    create UrlBeaconCardType and ImageBeaconCardType

--- ChimpNoise/BeaconCardView.m
+++ ChimpNoise/BeaconCardView.m
@@ -54,19 +54,20 @@
         self.containedTypeView = nil;
     }
     CGRect frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
-    self.containedTypeView = [self imageBeaconCardType:self.beacon frame: frame];
+    
+    if(self.beacon.type == nil){
+        self.containedTypeView = [self imageBeaconCardType:self.beacon frame:frame];
+    }
+    else if([self.beacon.type isEqualToString:@"url"]){
+        self.containedTypeView = [self urlBeaconCardType:self.beacon frame:frame];
+    }
+    else if ([self.beacon.type isEqualToString:@"image"]){
+        self.containedTypeView = [self imageBeaconCardType:self.beacon frame:frame];
+    }
     [self addSubview: self.containedTypeView];
     
     [self addTimer];
     [self startTimer];
-}
-
--(void) addImage:(NSString *) imageUrlString owner:(UIView *)ownerView{
-    UIImageView * imageView = [[UIImageView alloc] init];
-    imageView.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height * 10/10);
-    [imageView sd_setImageWithURL:[NSURL URLWithString: imageUrlString]
-                      placeholderImage:[UIImage imageNamed:@"placeholder.png"]];
-    [ownerView addSubview:imageView];
 }
 
 -(void) addTimer{
@@ -134,11 +135,76 @@
 }
 
 #pragma mark - BeaconCardTypes
--(UIView *) imageBeaconCardType:(AYBeacon *) beacon frame:(CGRect)frame{
-    UIView * imageBeaconCardType = [[UIView alloc]initWithFrame:frame];
-    [self addImage: beacon.imageURL owner:imageBeaconCardType];
+-(UIView *) urlBeaconCardType:(AYBeacon *)beacon frame:(CGRect)frame{
+    UIView *container = [[UIView alloc]initWithFrame:frame];
     
-    return imageBeaconCardType;
+    UIImageView * imageView = [[UIImageView alloc] init];
+    imageView.frame = CGRectMake(0,
+                                 0,
+                                 self.frame.size.width,
+                                 self.frame.size.height * 33/100);
+    [imageView sd_setImageWithURL:[NSURL URLWithString: beacon.imageURL]
+                 placeholderImage:[UIImage imageNamed:@"placeholder.png"]];
+    [container addSubview:imageView];
+    
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(20,
+                                                               self.frame.size.height * 36/100,
+                                                               container.frame.size.width - 40,
+                                                               self.frame.size.height * 11/100)];
+    title.text = @"James tendría que pagar 50 mil euros en multas por incidente vial";
+    title.font = [title.font fontWithSize:18];
+//    title.backgroundColor = [UIColor grayColor];
+    title.textColor = [UIColor colorWithRed:0.125 green:0.722 blue:0.902 alpha:1];;
+    title.numberOfLines = 2;
+    title.adjustsFontSizeToFitWidth = YES;
+    title.minimumScaleFactor = 10.0f/12.0f;
+    title.clipsToBounds = YES;
+    title.textAlignment = NSTextAlignmentLeft;
+    [container addSubview:title];
+    
+    UILabel *description = [[UILabel alloc] initWithFrame:CGRectMake(20,
+                                                                     self.frame.size.height * 46/100,
+                                                                     container.frame.size.width - 40,
+                                                                     self.frame.size.height * 26/100)];
+    description.text = @"El incidente de transito que protagonizó James Rodríguez el primer día de 2016 podría afectar la economía del colombiano.  Y es que según el diario ‘El Mundo’, de España, el ‘10’ tendría que pagar 50 mil euros sumando diferentes multas. \n\n www.futbolred.com";
+    description.font = [description.font fontWithSize:15];
+//    description.backgroundColor = [UIColor redColor];
+    description.textColor = [UIColor grayColor];
+    description.numberOfLines = 8;
+    description.adjustsFontSizeToFitWidth = YES;
+    description.minimumScaleFactor = 10.0f/12.0f;
+    description.clipsToBounds = YES;
+    description.textAlignment = NSTextAlignmentLeft;
+    [container addSubview:description];
+    
+    UILabel *tapToLearnMoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(20,
+                                                                     self.frame.size.height * 91/100,
+                                                                     container.frame.size.width - 40,
+                                                                     self.frame.size.height * 9/100)];
+    tapToLearnMoreLabel.text = @"Tap to learn more";
+    tapToLearnMoreLabel.font = [tapToLearnMoreLabel.font fontWithSize:12];
+//    tapToLearnMoreLabel.backgroundColor = [UIColor redColor];
+    tapToLearnMoreLabel.textColor = [UIColor grayColor];
+    tapToLearnMoreLabel.numberOfLines = 5;
+    tapToLearnMoreLabel.adjustsFontSizeToFitWidth = YES;
+    tapToLearnMoreLabel.minimumScaleFactor = 10.0f/12.0f;
+    tapToLearnMoreLabel.clipsToBounds = YES;
+    tapToLearnMoreLabel.textAlignment = NSTextAlignmentLeft;
+    [container addSubview:tapToLearnMoreLabel];
+    
+    return container;
+}
+
+-(UIView *) imageBeaconCardType:(AYBeacon *)beacon frame:(CGRect)frame{
+    UIView * container = [[UIView alloc]initWithFrame:frame];
+    
+    UIImageView * imageView = [[UIImageView alloc] init];
+    imageView.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height * 10/10);
+    [imageView sd_setImageWithURL:[NSURL URLWithString: beacon.imageURL]
+                 placeholderImage:[UIImage imageNamed:@"placeholder.png"]];
+    [container addSubview:imageView];
+
+    return container;
 }
 
 
