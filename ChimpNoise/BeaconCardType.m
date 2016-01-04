c2527e77ab5435450c76d5b05c72cc03132d1633
Andres Yepes <yepes07@gmail.com>
2016-01-04 16:52:50 -0500

    Implement Strategy pattern to create BeaconCardViews

---
+++ ChimpNoise/BeaconCardType.m
@@ -0,0 +1,13 @@
+//
+//  BeaconCardType.m
+//  ChimpNoise
+//
+//  Created by Andres Yepes on 1/4/16.
+//  Copyright © 2016 Andres Yepes. All rights reserved.
+//
+
+#import "BeaconCardType.h"
+
+@implementation BeaconCardType
+
+@end
---
+++ ChimpNoise/ImageBeaconCardType.h
@@ -0,0 +1,13 @@
+//
+//  ImageBeaconCardType.h
+//  ChimpNoise
+//
+//  Created by Andres Yepes on 1/4/16.
+//  Copyright © 2016 Andres Yepes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BeaconCardType.h"
+
+@interface ImageBeaconCardType : BeaconCardType
+@end
